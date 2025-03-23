ebee519542a0b1eafede2e89c35735429e625742
VlasovaNatalya <natatavv@gmail.com>
2025-03-23 18:22:36 +0700

    исправляет отображение типа

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -48,7 +48,7 @@ const Card = ({offer, block}: CardProps): JSX.Element => {
         <h2 className="place-card__name">
           <Link to={`${AppRoute.Offer}/${id}`}>{title}</Link>
         </h2>
-        <p className="place-card__type">{type}</p>
+        <p className="place-card__type">{type.charAt(0).toUpperCase() + type.slice(1)}</p>
       </div>
     </article>
   );
