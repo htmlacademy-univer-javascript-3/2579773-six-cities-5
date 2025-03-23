547e0cb268cd94ad62388d3c4cb78a80ad0d965c
VlasovaNatalya <natatavv@gmail.com>
2025-03-24 00:50:00 +0700

    добавляет работу с формой

---
+++ src/components/comment-form/comment-form.tsx
@@ -0,0 +1,55 @@
+import { useState, ChangeEvent, Fragment } from 'react';
+
+const ratingTitle: { [key: string]: string } = {
+  '5': 'perfect',
+  '4': 'good',
+  '3': 'not bad',
+  '2': 'badly',
+  '1': 'terribly'
+};
+
+const CommentForm = (): JSX.Element => {
+  const [comment, setComment] = useState('');
+  const [rating, setRating] = useState<string>('');
+
+  const isValid = comment.length >= 50 && rating !== '';
+
+  function handleCommentChange(evt: ChangeEvent<HTMLTextAreaElement>) {
+    setComment(evt.target.value);
+  }
+
+  function handleRatingChange(evt: ChangeEvent<HTMLInputElement>) {
+    setRating(evt.target.value);
+  }
+
+  return (
+    <form className="reviews__form form" action="#" method="post">
+      <label className="reviews__label form__label" htmlFor="review">Your review</label>
+      <div className="reviews__rating-form form__rating">
+        {Object.entries(ratingTitle)
+          .reverse()
+          .map(([score, title]) => (
+            <Fragment key={score}>
+              <input className="form__rating-input visually-hidden" name="rating" value={score} id={`${score}-stars`} type="radio" onChange={handleRatingChange} checked={rating === score}/>
+              <label htmlFor={`${score}-stars`} className="reviews__rating-label form__rating-label" title={title}>
+                <svg className="form__star-image" width="37" height="33">
+                  <use xlinkHref="#icon-star"></use>
+                </svg>
+              </label>
+            </Fragment>
+          ))}
+      </div>
+      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" value={comment} onChange={handleCommentChange}></textarea>
+      <div className="reviews__button-wrapper">
+        <p className="reviews__help">
+          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+        </p>
+        <button className="reviews__submit form__submit button" type="submit" disabled={!isValid}>
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default CommentForm;
--- src/components/offer-page/offer-page.tsx
+++ src/components/offer-page/offer-page.tsx
@@ -2,6 +2,7 @@ import { Link, Navigate, useParams } from 'react-router-dom';
 import Logo from '../logo/logo';
 import { OfferType } from '../../types/offer';
 import { AppRoute } from '../../const';
+import CommentForm from '../comment-form/comment-form';
 
 type OfferPageProps = {
   offers: OfferType[];
@@ -47,7 +48,7 @@ const OfferPage = ({offers}: OfferPageProps): JSX.Element => {
           <div className="offer__gallery-container container">
             <div className="offer__gallery">
               {offer.images.map((image) => (
-                <div className="offer__image-wrapper" key={offer.id}>
+                <div className="offer__image-wrapper" key={crypto.randomUUID()}>
                   <img className="offer__image" src={image} alt={`Photo of ${offer.title}`} />
                 </div>
               ))}
@@ -97,7 +98,7 @@ const OfferPage = ({offers}: OfferPageProps): JSX.Element => {
                 <h2 className="offer__inside-title">What&apos;s inside</h2>
                 <ul className="offer__inside-list">
                   {offer.goods.map((item) => (
-                    <li className="offer__inside-item" key={offer.id}>
+                    <li className="offer__inside-item" key={crypto.randomUUID()}>
                       {item}
                     </li>
                   ))}
@@ -131,7 +132,7 @@ const OfferPage = ({offers}: OfferPageProps): JSX.Element => {
                         <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar"/>
                       </div>
                       <span className="reviews__user-name">
-                    Max
+                        Max
                       </span>
                     </div>
                     <div className="reviews__info">
@@ -148,53 +149,9 @@ const OfferPage = ({offers}: OfferPageProps): JSX.Element => {
                     </div>
                   </li>
                 </ul>
-                <form className="reviews__form form" action="#" method="post">
-                  <label className="reviews__label form__label" htmlFor="review">Your review</label>
-                  <div className="reviews__rating-form form__rating">
-                    <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"/>
-                    <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
-                      </svg>
-                    </label>
-
-                    <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"/>
-                    <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
-                      </svg>
-                    </label>
-
-                    <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"/>
-                    <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
-                      </svg>
-                    </label>
-
-                    <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"/>
-                    <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
-                      </svg>
-                    </label>
-
-                    <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"/>
-                    <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star"></use>
-                      </svg>
-                    </label>
-                  </div>
-                  <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
-                  <div className="reviews__button-wrapper">
-                    <p className="reviews__help">
-                  To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-                    </p>
-                    <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
-                  </div>
-                </form>
+                <CommentForm />
               </section>
+
             </div>
           </div>
           <section className="offer__map map"></section>
