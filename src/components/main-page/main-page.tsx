f78f84667cf92498d542fb65cbea31292c616f7a
VlasovaNatalya <natatavv@gmail.com>
2025-03-27 13:23:31 +0700

    исправляет навигацию

--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -30,9 +30,9 @@ const MainPage = ({offers}: MainPageProps): JSX.Element => {
                   </Link>
                 </li>
                 <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
+                  <Link className="header__nav-link" to={AppRoute.Login}>
                     <span className="header__signout">Sign out</span>
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </nav>
