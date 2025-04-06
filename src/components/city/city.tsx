f3784a8eb78053e0454f8cccd433c4abfe73aa38
VlasovaNatalya <natatavv@gmail.com>
2025-04-06 18:10:39 +0700

    исправляет обработчик

--- src/components/city/city.tsx
+++ src/components/city/city.tsx
@@ -6,17 +6,11 @@ type CityProps = {
   onClick: () => void;
 }
 
-const City = ({city, isActive, onClick}: CityProps): JSX.Element => {
-  function handleCityChange () {
-    onClick();
-  }
-
-  return (
-    <li className="locations__item">
-      <a className={`locations__item-link tabs__item ${isActive ? 'tabs__item--active' : ''}`} href="#" onClick={handleCityChange}>
-        <span>{city.name}</span>
-      </a>
-    </li>
-  );
-};
+const City = ({city, isActive, onClick}: CityProps): JSX.Element => (
+  <li className="locations__item">
+    <a className={`locations__item-link tabs__item ${isActive ? 'tabs__item--active' : ''}`} href="#" onClick={() => onClick()}>
+      <span>{city.name}</span>
+    </a>
+  </li>
+);
 export default City;
