266c4a5960440ef14286ba58894fa2e23b924d00
VlasovaNatalya <natatavv@gmail.com>
2025-04-14 16:13:07 +0700

    добавляет интерцептор для токена

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,20 @@
-import axios, {AxiosInstance} from 'axios';
+import axios, {AxiosInstance, InternalAxiosRequestConfig} from 'axios';
 import { BASE_URL, TIMEOUT } from '../const';
+import { getToken } from './token';
 
 const createApi = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BASE_URL,
     timeout: TIMEOUT,
+  });
+
+  api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+    const token = getToken();
+
+    if (token && config.headers) {
+      config.headers['x-token'] = token;
+    }
+    return config;
   });
 
   return api;
