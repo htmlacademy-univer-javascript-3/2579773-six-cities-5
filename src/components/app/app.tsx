1ce2ad0ac7458afb11d84cfa3f3d5c774effc4e8
VlasovaNatalya <natatavv@gmail.com>
2025-04-15 22:57:56 +0700

    исправляет рендер роутера и спиннера

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import { AppRoute, AuthorizationStatus } from '../../const';
+import { AppRoute } from '../../const';
 import MainPage from '../main-page/main-page';
 import FavoritesPage from '../favorites-page/favorites-page';
 import LoginPage from '../login-page/login-page';
@@ -7,49 +7,38 @@ import OfferPage from '../offer-page/offer-page';
 import ErrorPage from '../error-page/error-page';
 import PrivateRoute from '../private-route/private-route';
 import { CityType } from '../../types/city';
-import { useAppSelector } from '../../hooks';
-import Spinner from '../../spinner/spinner';
-import { State } from '../../types/state';
 
 type AppScreenProps = {
   cities: CityType[];
 }
 
-const App = ({cities}: AppScreenProps): JSX.Element => {
-  const authorizationStatus = useAppSelector((state: State) => state.authorizationStatus);
-
-  if (authorizationStatus === AuthorizationStatus.Unknown) {
-    return <Spinner />;
-  }
-
-  return(
-    <Routes>
-      <Route
-        path = {AppRoute.Main}
-        element = {<MainPage cities={cities}/>}
-      />
-      <Route
-        path = {AppRoute.Favorites}
-        element = {
-          <PrivateRoute>
-            <FavoritesPage />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path = {AppRoute.Login}
-        element ={<LoginPage />}
-      />
-      <Route
-        path = {`${AppRoute.Offer}/:id`}
-        element ={<OfferPage />}
-      />
-      <Route
-        path = "*"
-        element ={<ErrorPage />}
-      />
-    </Routes>
-  );
-};
+const App = ({cities}: AppScreenProps): JSX.Element => (
+  <Routes>
+    <Route
+      path = {AppRoute.Main}
+      element = {<MainPage cities={cities}/>}
+    />
+    <Route
+      path = {AppRoute.Favorites}
+      element = {
+        <PrivateRoute>
+          <FavoritesPage />
+        </PrivateRoute>
+      }
+    />
+    <Route
+      path = {AppRoute.Login}
+      element ={<LoginPage />}
+    />
+    <Route
+      path = {`${AppRoute.Offer}/:id`}
+      element ={<OfferPage />}
+    />
+    <Route
+      path = "*"
+      element ={<ErrorPage />}
+    />
+  </Routes>
+);
 
 export default App;
--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -2,6 +2,7 @@ import { Navigate } from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from '../../const';
 import { useAppSelector } from '../../hooks';
 import { State } from '../../types/state';
+import Spinner from '../../spinner/spinner';
 
 type PrivateRouteProps = {
   children: JSX.Element;
@@ -10,6 +11,10 @@ type PrivateRouteProps = {
 const PrivateRoute = (props: PrivateRouteProps): JSX.Element => {
   const {children} = props;
   const authorizationStatus = useAppSelector((state: State) => state.authorizationStatus);
+
+  if (authorizationStatus === AuthorizationStatus.Unknown) {
+    return <Spinner />;
+  }
 
   return (
     authorizationStatus === AuthorizationStatus.Auth ? children : <Navigate to={AppRoute.Login} />
