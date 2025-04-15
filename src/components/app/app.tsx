12d085a576fa8800eba647d08ea9a7403752a2e3
VlasovaNatalya <natatavv@gmail.com>
2025-04-15 15:08:04 +0700

    переместил роутер, чтобы избежать лишних перерендеров

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -7,59 +7,48 @@ import OfferPage from '../offer-page/offer-page';
 import ErrorPage from '../error-page/error-page';
 import PrivateRoute from '../private-route/private-route';
 import { CityType } from '../../types/city';
-import { useAppDispatch, useAppSelector } from '../../hooks';
-import { useEffect } from 'react';
-import { checkAuthAction } from '../../store/api-actions';
+import { useAppSelector } from '../../hooks';
 import Spinner from '../../spinner/spinner';
 import { State } from '../../types/state';
-import browserHistory from '../../browser-history';
-import HistoryRouter from '../history-route/history-route';
 
 type AppScreenProps = {
   cities: CityType[];
 }
 
 const App = ({cities}: AppScreenProps): JSX.Element => {
-  const dispatch = useAppDispatch();
   const authorizationStatus = useAppSelector((state: State) => state.authorizationStatus);
-
-  useEffect(() => {
-    dispatch(checkAuthAction());
-  }, [dispatch]);
 
   if (authorizationStatus === AuthorizationStatus.Unknown) {
     return <Spinner />;
   }
 
   return(
-    <HistoryRouter history={browserHistory}>
-      <Routes>
-        <Route
-          path = {AppRoute.Main}
-          element = {<MainPage cities={cities}/>}
-        />
-        <Route
-          path = {AppRoute.Favorites}
-          element = {
-            <PrivateRoute>
-              <FavoritesPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path = {AppRoute.Login}
-          element ={<LoginPage />}
-        />
-        <Route
-          path = {`${AppRoute.Offer}/:id`}
-          element ={<OfferPage />}
-        />
-        <Route
-          path = "*"
-          element ={<ErrorPage />}
-        />
-      </Routes>
-    </HistoryRouter>
+    <Routes>
+      <Route
+        path = {AppRoute.Main}
+        element = {<MainPage cities={cities}/>}
+      />
+      <Route
+        path = {AppRoute.Favorites}
+        element = {
+          <PrivateRoute>
+            <FavoritesPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path = {AppRoute.Login}
+        element ={<LoginPage />}
+      />
+      <Route
+        path = {`${AppRoute.Offer}/:id`}
+        element ={<OfferPage />}
+      />
+      <Route
+        path = "*"
+        element ={<ErrorPage />}
+      />
+    </Routes>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,24 @@ import App from './components/app/app';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { cities } from './mocks/cities';
+import HistoryRouter from './components/history-route/history-route';
+import browserHistory from './browser-history';
+import { checkAuthAction } from './store/api-actions';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+store.dispatch(checkAuthAction());
+
 root.render(
   <React.StrictMode>
-    <Provider store = {store}>
-      <App
-        cities={cities}
-      />
-    </Provider>
+    <HistoryRouter history={browserHistory}>
+      <Provider store = {store}>
+        <App
+          cities={cities}
+        />
+      </Provider>
+    </HistoryRouter>
   </React.StrictMode>
 );
